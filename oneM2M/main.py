989b11ba305cbccc34df3b49349a6b93e67f3f7b
huanpc <nauh94@gmail.com>
2016-07-12 21:02:58 +0700

    Use `rabbitpy` to communicate with CloudAMQP

--- oneM2M/main.py
+++ oneM2M/main.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 __author__ = 'huanpc'
 
 import asyncio
@@ -11,6 +12,8 @@ from aiohttp import web
 import logging
 
 import sys
+
+import cloudAMPQclient
 
 PROTOCOL = 'http'
 HOST = '127.0.0.1'
@@ -36,10 +39,22 @@ def get_resource_state(request):
     return web.Response(status=200, body=response.read().decode().encode('utf-8'))
 
 @asyncio.coroutine
-def switchON_resource(request):
+def get_resource_description(request):
+    app_id = request.match_info.get('app_id')
+    # http://127.0.0.1:8080/~/mn-cse/mn-name/MY_SENSOR/DESCRIPTOR/ol
+    resource_uri = '/~/' + 'mn-cse/mn-name' + '/' + app_id + '/' + 'DESCRIPTOR' + '/' + 'la'
+    con = http.client.HTTPConnection(DOMAIN)
+    header = {'X-M2M-Origin': 'admin:admin', "Content-type": "application/xml"}
+    con.request('GET', resource_uri, '', header)
+    response = con.getresponse()
+    logger.info("Request: GET/URI:" + resource_uri)
+    return web.Response(status=200, body=response.read().decode().encode('utf-8'))
+
+@asyncio.coroutine
+def switchON_lamp(request):
     app_id = request.match_info.get('app_id')
     # /mn-cse/mn-name/LAMP_0?op=setOn&lampid=LAMP_0
-    resource_uri = '/~/' + '/mn-cse/mn-name/' + app_id + '?op=setOn&lampid=' + app_id
+    resource_uri = '/~/' + 'mn-cse/mn-name/' + app_id + '?op=setOn&lampid=' + app_id
     con = http.client.HTTPConnection(DOMAIN)
     header = {'X-M2M-Origin': 'admin:admin', "Content-type": "application/xml"}
     con.request('POST', resource_uri, '', header)
@@ -48,10 +63,10 @@ def switchON_resource(request):
     return web.Response(status=200, body=response.read().decode().encode('utf-8'))
 
 @asyncio.coroutine
-def switchOFF_resource(request):
+def switchOFF_lamp(request):
     app_id = request.match_info.get('app_id')
     # /mn-cse/mn-name/LAMP_0?op=setOff&lampid=LAMP_0
-    resource_uri = '/~/' + '/mn-cse/mn-name/' + app_id + '?op=setOff&lampid=' + app_id
+    resource_uri = '/~/' + 'mn-cse/mn-name/' + app_id + '?op=setOff&lampid=' + app_id
     con = http.client.HTTPConnection(DOMAIN)
     header = {'X-M2M-Origin': 'admin:admin', "Content-type": "application/xml"}
     con.request('POST', resource_uri, '', header)
@@ -60,35 +75,50 @@ def switchOFF_resource(request):
     return web.Response(status=200, body=response.read().decode().encode('utf-8'))
 
 @asyncio.coroutine
-def create_application(request):
-    uri = 'http://127.0.0.1:8080/~/in-cse'
-    method = 'POST'
-    headers = {'X-M2M-Origin': 'admin:admin', 'Content-Type': 'application/xml;ty=2', 'X-M2M-NM': 'MY_SENSOR'}
-    body = '''
-    <om2m:ae xmlns:om2m="http://www.onem2m.org/xml/protocols">
-        <api>app-sensor</api>
-        <lbl>Type/sensor Category/temperature Location/home</lbl>
-        <rr>false</rr>
-    </om2m:ae>
-    '''
-    con = http.client.HTTPConnection(DOMAIN)
-    con.request(method, uri, '', header)
-    response = con.getresponse()
-    logger.info("Request: POST/URI:" + resource_uri)
-    return web.Response(status=200, body=response.read().decode().encode('utf-8'))
+def monitor(request):
+    data = yield from request.text()
+    logger.info('---> Publish message to CloudAMPQ')
+    cloudAMPQclient.publish_message(data)
+    return web.Response(status=204, body=' '.encode('utf-8'))
+
+@asyncio.coroutine
+def tracking(request):
+    logger.info('---> Cosume message from CloudAMPQ')
+    message = cloudAMPQclient.consume_message()
+    return web.Response(status=200, body=message.encode('utf-8'))
+
+# @asyncio.coroutine
+# def create_application_entity(request):
+#     uri = 'http://127.0.0.1:8080/~/in-cse'
+#     method = 'POST'
+#     headers = {'X-M2M-Origin': 'admin:admin', 'Content-Type': 'application/xml;ty=2', 'X-M2M-NM': 'MY_SENSOR'}
+#     body = '''
+#     <om2m:ae xmlns:om2m="http://www.onem2m.org/xml/protocols">
+#         <api>app-sensor</api>
+#         <lbl>Type/sensor Category/temperature Location/home</lbl>
+#         <rr>false</rr>
+#     </om2m:ae>
+#     '''
+#     con = http.client.HTTPConnection(DOMAIN)
+#     con.request(method, uri, '', headers)
+#     response = con.getresponse()
+#     logger.info("Request: POST/URI:" + uri)
+#     return web.Response(status=200, body=response.read().decode().encode('utf-8'))
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
-    # Get resource info
+    # Get resource description
+    app.router.add_route('GET', '/resource/{app_id}/descriptor', get_resource_description)
+    # Get resource state
     app.router.add_route('GET', '/resource/{app_id}/state', get_resource_state)
-    # Switch on resource
-    app.router.add_route('GET', '/resource/{app_id}/switchON', get_resource_state)
-    # Switch off resource
-    app.router.add_route('GET', '/resource/{app_id}/switchOFF', get_resource_state)
-    # Create a resource
-    app.router.add_route('GET', '/resource/create', get_resource_state)	
-    
+    # Switch on lamp resource
+    app.router.add_route('GET', '/resource/{app_id}/switchON', switchON_lamp)
+    # Switch off lamp resource
+    app.router.add_route('GET', '/resource/{app_id}/switchOFF', switchOFF_lamp)
+
+    app.router.add_route('POST', '/monitor', monitor)
+
     srv = yield from loop.create_server(app.make_handler(), HOST, PORT)
     print("Server started at " + HOST + ":" + str(PORT))
     return srv
